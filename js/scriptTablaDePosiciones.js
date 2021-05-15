9408a4a63e41f6538224cbdd3a49744e83b87a38
noebenitez <noebenitez_03@hotmail.com>
2021-05-15 03:40:43 -0300

    Tabla de posiciones con logos y estilos de las 5 ligas
    Se agrega la carpeta 'assets' para usar los estilos

--- js/scriptTablaDePosiciones.js
+++ js/scriptTablaDePosiciones.js
@@ -1,10 +1,10 @@
 const apiKey = "549a7bb64cmsh22a289e69814bbcp1e7974jsnc74d4dcbd570";
 
 const endpointPosicionesLigaEspaña = "standings?season=2020&league=140";
-const endpointPosicionesLigaInglaterra = "";
-const endpointPosicionesLigaAlemania = "";
-const endpointPosicionesLigaItalia = "";
-const endpointPosicionesLigaFrancia = "";
+const endpointPosicionesLigaInglaterra = "standings?season=2020&league=39";
+const endpointPosicionesLigaAlemania = "standings?season=2020&league=78";
+const endpointPosicionesLigaItalia = "standings?season=2020&league=135";
+const endpointPosicionesLigaFrancia = "standings?season=2020&league=61";
 
 const equiposPrimeraDivisionEspania = new Array();
 const equiposPrimeraDivisionInglaterra = new Array();
@@ -12,7 +12,40 @@ const equiposPrimeraDivisionAlemania = new Array();
 const equiposPrimeraDivisionItalia = new Array();
 const equiposPrimeraDivisionFrancia = new Array();
 
+
+
 function callApi(endPoint) {
+
+    return new Promise(function (resolve, reject) {
+
+        var request = new XMLHttpRequest();
+
+        request.open("GET", "https://api-football-beta.p.rapidapi.com/" + endPoint);
+        request.setRequestHeader("x-rapidapi-key", apiKey);
+        request.setRequestHeader("x-rapidapi-host", "api-football-beta.p.rapidapi.com");
+
+        request.responseType = 'json';
+
+        request.onload = function () {
+
+            if (request.status == 200) {
+                resolve(request.response);
+            }
+            else {
+                reject(Error('Json couldn\'t be loaded. Error: ' + request.statusText));
+            }
+        }
+
+        request.onerror = function () {
+            reject(Error('Oops!, there was a network error.'));
+        }
+
+        request.send();
+    });
+
+}
+
+function callApi2(endPoint) {
 
     const data = null;
 
@@ -20,34 +53,33 @@ function callApi(endPoint) {
     //xhr.withCredentials = true;
 
     return new Promise((resolve, reject) => {
-    
-    xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === this.DONE) {
-            resolve(this.responseText);
-        }
-    });
 
-    xhr.open("GET", "https://api-football-beta.p.rapidapi.com/"+endPoint);
-    xhr.setRequestHeader("x-rapidapi-key", apiKey);
-    xhr.setRequestHeader("x-rapidapi-host", "api-football-beta.p.rapidapi.com");
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === this.DONE) {
+                resolve(this.responseText);
+            }
+        });
 
-    xhr.send(data);
+        xhr.open("GET", "https://api-football-beta.p.rapidapi.com/" + endPoint);
+        xhr.setRequestHeader("x-rapidapi-key", apiKey);
+        xhr.setRequestHeader("x-rapidapi-host", "api-football-beta.p.rapidapi.com");
+
+        xhr.send(data);
 
     });
 
 }
-
 
 function cleanTable() {
 
     var bodyTable = document.getElementById("bodyTable");
     var headTable = document.getElementById("headTable");
 
-    while(bodyTable.firstChild) {
+    while (bodyTable.firstChild) {
         bodyTable.removeChild(bodyTable.firstChild);
     }
 
-    while(headTable.firstChild) {
+    while (headTable.firstChild) {
         headTable.removeChild(headTable.firstChild);
     }
 }
@@ -78,21 +110,28 @@ function insertarEquipoEnLaTabla(equipo) {
     var bodyTable = document.getElementById("bodyTable");
 
     var tr = document.createElement("tr");
-    tr.classList.add("table-active");
+    tr.classList.add("tbody-dark");
     bodyTable.appendChild(tr);
 
     var tdRank = document.createElement("td");
-    tdRank.classList.add("table-active");
+    tdRank.classList.add("tbody-dark");
     tr.appendChild(tdRank);
     tdRank.innerHTML = equipo["rank"];
 
     var tdName = document.createElement("td");
-    tdName.classList.add("table-active");
+    tdName.classList.add("tbody-dark");
     tr.appendChild(tdName);
-    tdName.innerHTML = equipo["team"]["name"];
+    tdName.innerHTML = `<div class="media align-items-center">
+                            <a href="#" class="avatar img mr-3">
+                                <img alt="Team logo" src=" ${equipo["team"]["logo"]}">
+                            </a>
+                            <div class="media-body">
+                                <span class="name mb-0 text-sm\">${equipo["team"]["name"]}</span>
+                            </div>
+                        </div>`;
 
     var tdPuntos = document.createElement("td");
-    tdPuntos.classList.add("table-active");
+    tdPuntos.classList.add("tbody-dark");
     tr.appendChild(tdPuntos);
     tdPuntos.innerHTML = equipo["points"];
 
@@ -100,7 +139,7 @@ function insertarEquipoEnLaTabla(equipo) {
 
 
 window.onload = () => {
-    
+
     const buttonLaLiga = document.getElementById("LA_LIGA");
     const buttonSerieA = document.getElementById("SERIE_A");
     const buttonBundesliga = document.getElementById("BUNDESLIGA");
@@ -108,14 +147,13 @@ window.onload = () => {
     const buttonPremierLeague = document.getElementById("PREMIER_LEAGUE");
 
 
-
-    buttonLaLiga.addEventListener("click",() => {
+    buttonLaLiga.addEventListener("click", () => {
         callApi(endpointPosicionesLigaEspaña)
-            
+
             .then(response => {
 
-                equiposPrimeraDivisionEspania.push(JSON.parse(response)["response"][0]["league"]["standings"][0]);
-                          
+                equiposPrimeraDivisionEspania.push(response["response"][0]["league"]["standings"][0]);
+                
                 cleanTable();
                 agregarHeadTable();
 
@@ -124,24 +162,86 @@ window.onload = () => {
                 });
 
             })
-            .catch(error => console.log(error))
+        .catch(error => console.log(error))
     });
 
-    buttonSerieA.addEventListener("click",() => {
+    buttonSerieA.addEventListener("click", () => {
 
+        callApi(endpointPosicionesLigaItalia)
+
+            .then(response => {
+
+                equiposPrimeraDivisionItalia.push(response["response"][0]["league"]["standings"][0]);
+
+                cleanTable();
+                agregarHeadTable();
+
+                equiposPrimeraDivisionItalia[0].forEach(equipo => {
+                    insertarEquipoEnLaTabla(equipo);
+                });
+
+            })
+        .catch(error => console.log(error));
+     });
+
+    buttonBundesliga.addEventListener("click", () => {
+
+        callApi(endpointPosicionesLigaAlemania)
+
+            .then(response => {
+
+                equiposPrimeraDivisionAlemania.push(response["response"][0]["league"]["standings"][0]);
+
+                cleanTable();
+                agregarHeadTable();
+
+                equiposPrimeraDivisionAlemania[0].forEach(equipo => {
+                    insertarEquipoEnLaTabla(equipo);
+                });
+
+            })
+        .catch(error => console.log(error));
     });
 
-    buttonBundesliga.addEventListener("click",() => {
+    buttonLeague1.addEventListener("click", () => {
 
+        callApi(endpointPosicionesLigaFrancia)
+
+            .then(response => {
+
+                equiposPrimeraDivisionFrancia.push(response["response"][0]["league"]["standings"][0]);
+
+                cleanTable();
+                agregarHeadTable();
+
+                equiposPrimeraDivisionFrancia[0].forEach(equipo => {
+                    insertarEquipoEnLaTabla(equipo);
+                });
+
+            })
+        .catch(error => console.log(error));
     });
 
-    buttonLeague1.addEventListener("click",() => {
+    buttonPremierLeague.addEventListener("click", () => {
 
+        callApi(endpointPosicionesLigaInglaterra)
+
+            .then(response => {
+
+                equiposPrimeraDivisionInglaterra.push(response["response"][0]["league"]["standings"][0]);
+
+                cleanTable();
+                agregarHeadTable();
+
+                console.log(equiposPrimeraDivisionInglaterra);
+                equiposPrimeraDivisionInglaterra[0].forEach(equipo => {
+                    insertarEquipoEnLaTabla(equipo);
+                });
+
+            })
+        .catch(error => console.log(error));
     });
 
-    buttonPremierLeague.addEventListener("click",() => {
-
-    });
 
 }
 
